1238efb600196af355a44034d5c8155dca13ed98
Dennis Paust <46346469+dango301@users.noreply.github.com>
2021-03-14 16:15:53 +0100

    Add files via upload

---
+++ Sender/Light/Light.ino
@@ -0,0 +1,54 @@
+int T; // data transfer rate in ms/bit
+
+boolean first = true;
+long lastCall = 0;
+String bits = "";
+int i = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void light(boolean state) {
+  digitalWrite(8, state);
+  digitalWrite(LED_BUILTIN, state);
+}
+
+void(* reset) (void) = 0; // function to reset program
+void restart() {
+  light(0);
+  reset();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    if (bits == "" || bits == "RESET") {
+      String rawIn = Serial.readString();
+      freq = rawIn.substring(0, 3).toInt();
+      bits = rawIn.substring(3);
+    } else if (Serial.readString() == "RESET") restart();
+  }
+
+
+  if (bits != "" && millis() - lastCall >= T) {
+    if (first) {
+      lastCall = millis();
+      first = false;
+    }
+    else
+      lastCall += T; // for more accuracy
+
+    boolean state = bits.charAt(i) == '1';
+    Serial.write(state ? 1 : 0); // Cheat
+    light(state);
+    Serial.println(i);
+
+    if (++i > bits.length()) {
+      restart();
+    }
+  }
+}
+
+// MERKE: öffnen der seriellen Monitors setzt Arduino Programm zurück
